73949b71f5378e071e1b9948be66bd2cfac43f43
marzia <you@example.com>
2025-06-27 17:30:33 +0200

    Refactor code structure for improved readability and maintainability

--- server.js
+++ server.js
@@ -2,10 +2,24 @@ const express = require("express");
 const app = express();
 const port = 3000;
 
+/**
 app.get("/", (req, res) => {
   res.send("Hello World!");
+}); */
+
+app.get("/", (req, res) => {
+  res.send("Server del mio blog");
 });
 
 app.listen(port, () => {
   console.log(`Example app listening on http://localhost: ${port}`);
 });
+
+const posts = [
+  {
+    title: " Ciambellone",
+    recipe: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+    img: 
+    tags: ["torta","fatto a mano", "ricetta veloce", "cioccolato" ]
+  },
+];
