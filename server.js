7f51923029c56de0bc0b4c3102ea9c74986c80b1
marzia <you@example.com>
2025-06-27 17:45:50 +0200

    Add static file serving and fix typo in posts API response

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+app.use(express.static("public"));
 
 /**
 app.get("/", (req, res) => {
@@ -52,3 +53,7 @@ const posts = [
     tags: ["torta", "fatto a mano", "ricetta veloce", "cioccolato"],
   },
 ];
+
+app.get("/api/posts", (req, res) => {
+  res.jason(posts);
+});
